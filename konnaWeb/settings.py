ef1279eafa4bb4864d4d3b3c81a965445754a2d0
Hain Zuppur <h.zuppur@gmail.com>
2020-05-13 00:54:41 +0300

    moved secret key to env variables

--- konnaWeb/settings.py
+++ konnaWeb/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nj26g!8#*#hc6%y52%ruiykvt0)!_xxt++%p+*nkcxgvkpi*2j'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = [
   'delfi-comment-inspirational.herokuapp.com',
