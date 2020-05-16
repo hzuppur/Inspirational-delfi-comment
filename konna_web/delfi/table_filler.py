0af49c41d7af3250b307b5e11e44006f2ee92405
Hain Zuppur <h.zuppur@gmail.com>
2020-05-16 23:58:50 +0300

    table filler now handles IntegrityError

--- konna_web/delfi/table_filler.py
+++ konna_web/delfi/table_filler.py
@@ -1,6 +1,7 @@
 from konna_web.models import Article, Comment, CommentReply
 import konna_web.delfi.delfi_articles as da
 import konna_web.delfi.delfi_comments as dc
+from django.db import IntegrityError
 
 
 def add_articles_to_table():
@@ -13,12 +14,21 @@ def add_articles_to_table():
     # Get all comments for that article and iterate over them
     comments = dc.get_comments_with_replies(d_article["id"])
     for d_comment in comments:
-      # Check if comment has content and subject and save comment
-      if d_comment["subject"] is not None and d_comment["content"]:
-        comment = Comment(article=article, content=d_comment["content"], subject=d_comment["subject"])
-        comment.save()
+      # If the comment already exists, then it throws IntegrityError
+      try:
+        # Check if comment has content and subject and save comment
+        if d_comment["subject"] is not None and d_comment["content"]:
+          comment = Comment(article=article, content=d_comment["content"], subject=d_comment["subject"])
+          comment.save()
+      except IntegrityError:
+        pass
+
         # If comment has reply´s, add them to the reply table
         if d_comment["replies"] is not None:
+          # If the reply already exists, then it throws IntegrityError
           for d_reply in d_comment["replies"]:
-            reply = CommentReply(comment=comment, content=d_reply["content"], subject=d_reply["subject"])
-            reply.save()
+            try:
+              reply = CommentReply(comment=comment, content=d_reply["content"], subject=d_reply["subject"])
+              reply.save()
+            except IntegrityError:
+              pass
