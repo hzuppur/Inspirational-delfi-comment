2154235b7cd3624786c88e7c392ca7c111d1f89d
Hain Zuppur <h.zuppur@gmail.com>
2020-05-17 00:28:47 +0300

    Reeeee

--- konna_web/delfi/table_filler.py
+++ konna_web/delfi/table_filler.py
@@ -14,21 +14,12 @@ def add_articles_to_table():
     # Get all comments for that article and iterate over them
     comments = dc.get_comments_with_replies(d_article["id"])
     for d_comment in comments:
-      # If the comment already exists, then it throws IntegrityError
-      try:
-        # Check if comment has content and subject and save comment
-        if d_comment["subject"] is not None and d_comment["content"]:
-          comment = Comment(article=article, content=d_comment["content"], subject=d_comment["subject"])
-          comment.save()
-      except IntegrityError:
-        pass
+      # Check if comment has content and subject and save comment
+      if d_comment["subject"] is not None and d_comment["content"]:
+        comment, created = Comment.objects.get_or_create(article=article, content=d_comment["content"], subject=d_comment["subject"])
 
-        # If comment has reply´s, add them to the reply table
-        if d_comment["replies"] is not None:
-          # If the reply already exists, then it throws IntegrityError
-          for d_reply in d_comment["replies"]:
-            try:
-              reply = CommentReply(comment=comment, content=d_reply["content"], subject=d_reply["subject"])
-              reply.save()
-            except IntegrityError:
-              pass
+      # If comment has reply´s, add them to the reply table
+      if d_comment["replies"] is not None:
+        # If the reply already exists, then it throws IntegrityError
+        for d_reply in d_comment["replies"]:
+          reply, created = CommentReply.objects.get_or_create(comment=comment, content=d_reply["content"], subject=d_reply["subject"])
