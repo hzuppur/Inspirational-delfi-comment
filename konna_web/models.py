05185a9184650db159abc4a1fca71b3b59f7f5d6
Hain Zuppur <h.zuppur@gmail.com>
2020-05-16 23:46:22 +0300

    Added comment unique_together

--- konna_web/models.py
+++ konna_web/models.py
@@ -16,6 +16,9 @@ class Comment(models.Model):
   content = models.CharField(max_length=4000)
   subject = models.CharField(max_length=500)
 
+  class Meta:
+    unique_together = ["article", "content", "subject"]
+
   def __str__(self):
     return self.content
 
@@ -24,6 +27,9 @@ class CommentReply(models.Model):
   comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
   content = models.CharField(max_length=4000)
   subject = models.CharField(max_length=500)
+
+  class Meta:
+    unique_together = ["comment", "content", "subject"]
 
   def __str__(self):
     return self.content
